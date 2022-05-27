f0812d2950452f676795476f1276396608f30916
Pedro Henrique <62969781+pedrohbrandao@users.noreply.github.com>
2022-05-27 19:16:22 -0300

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,15 @@
 import { AuthService } from 'src/auth/auth.service';
 import { UsersService } from './users.service';
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
 
-    constructor(private usersService:UsersService, private AuthService:AuthService){}
-
+    constructor(private usersService: UsersService, private AuthService: AuthService) { }
+    
+    @UseGuards(JwtAuthGuard)
     @Get()
     async Getall() {
         return this.usersService.findall()
